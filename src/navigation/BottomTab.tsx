4bca7175db568d9ebfef4134137bc50c628383b1
gopi <>
2025-05-14 13:25:39 +0530

    Add test action

--- src/navigation/BottomTab.tsx
+++ src/navigation/BottomTab.tsx
@@ -1,15 +1,12 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import Dummy from '../screens/Dummy';
-import MainScreen from '../screens/MainScreen';
 
 const BottomTab = () => {
   const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator>
-      <Tab.Screen name="MainScreen" component={MainScreen} />
       <Tab.Screen name="Dummy" component={Dummy} />
     </Tab.Navigator>
   );
@@ -17,4 +14,3 @@ const BottomTab = () => {
 
 export default BottomTab;
 
-const styles = StyleSheet.create({});
---
+++ src/screens/Dummy.test.tsx
@@ -0,0 +1,6 @@
+describe('Dummy test', () => {
+    it('prints Hello, World!', () => {
+      console.log('Hello, World!');
+      expect(true).toBe(true); // dummy assertion
+    });
+  });
